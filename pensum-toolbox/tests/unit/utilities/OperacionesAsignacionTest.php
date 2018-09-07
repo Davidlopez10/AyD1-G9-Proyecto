2846cbd63e9b2468bf30f6d1a1acd4b25494cbe9
kevin <kevinalbertoorellana@gmail.com>
2018-09-07 11:52:37 -0600

    Correccion prueba de historia 6

--- pensum-toolbox/tests/unit/utilities/OperacionesAsignacionTest.php
+++ pensum-toolbox/tests/unit/utilities/OperacionesAsignacionTest.php
@@ -10,16 +10,14 @@ class OperacionesAsignacionTest extends \Codeception\Test\Unit
     private $usuario_de_pruebas = 209900909;
     private $usuario_inexistente = -1;
     private $estadistica_1 = '732';
-    private $idioma_tecnico_2 = '0008';
-    private $mate_basica_2 = '103';
-    private $mate_intermedia_3 = '114';
-    private $deportes_1 = '039';
+    private $idioma_tecnico_3 = '0009';
+    private $deportes_2 = '040'
 
 
 
 
     public function testTratarDeVisualizarAsignacionSinPrerrequisitos(){
-        $result = OperacionesCurso::get_cursos_disponibles_asignacion($this->usuario_de_pruebas, array($this->mate_intermedia_3));
+        $result = OperacionesCurso::get_cursos_disponibles_asignacion($this->usuario_de_pruebas, array($this->$deportes_2));
         $this->assertEmpty($result);
     }
 
@@ -28,7 +26,7 @@ class OperacionesAsignacionTest extends \Codeception\Test\Unit
     * @depends testTratarDeVisualizarAsignacionSinPrerrequisitos()
     */
     public function testTratarDeVisualizarAsignacion(){
-        $result = OperacionesCurso::get_cursos_disponibles_asignacion($this->usuario_de_pruebas, array($this->estadistica1, $this->$idioma_tecnico_2, $this->mate_basica_2));
+        $result = OperacionesCurso::get_cursos_disponibles_asignacion($this->usuario_de_pruebas, array($this->estadistica1, $this->$idioma_tecnico_3));
         $this->assertNotEmpty($result);
     }
 }
