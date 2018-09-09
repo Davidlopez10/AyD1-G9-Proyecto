c56ae7fda8909396a70617e4501898e922e25ea2
jairoHer <32321394+jairoHer@users.noreply.github.com>
2018-09-09 00:28:55 -0600

    Se agrego la prueba unitaria "Lista de actividades disponibles"
    Fallo de momento por que no se ha creado la funcion que la validara

--- db/actividadesExtracurriculares.sql
+++ db/actividadesExtracurriculares.sql
@@ -17,3 +17,19 @@ INSERT INTO curso (codigo, nombre, creditos, inicio_rama, obligatorio, area) val
 INSERT INTO curso (codigo, nombre, creditos, inicio_rama, obligatorio, area) values ('5007','Actividad extra 1',1,'N','N',8);
 INSERT INTO curso (codigo, nombre, creditos, inicio_rama, obligatorio, area) values ('5008','Actividad extra 2',1,'N','N',8);
 
+INSERT INTO usuario_curso (usuario, curso, estado_curso) VALUES ('209900909', '5000', 1);
+INSERT INTO usuario_curso (usuario, curso, estado_curso) VALUES ('209900909', '5001', 1);
+INSERT INTO usuario_curso (usuario, curso, estado_curso) VALUES ('209900909', '5002', 1);
+INSERT INTO usuario_curso (usuario, curso, estado_curso) VALUES ('209900909', '5003', 1);
+INSERT INTO usuario_curso (usuario, curso, estado_curso) VALUES ('209900909', '5004', 1);
+INSERT INTO usuario_curso (usuario, curso, estado_curso) VALUES ('209900909', '5005', 1);
+INSERT INTO usuario_curso (usuario, curso, estado_curso) VALUES ('209900909', '5006', 1);
+
+
+select * 
+from usuario_curso
+where curso in (select codigo 
+	from curso
+    where area=8);
+    
+SELECT nombre from curso where area = 8;
