a1d241442e3f1e4f86127b018c072db48018a5c6
david <dasensag@gmail.com>
2018-09-20 00:17:52 -0600

    Correccion de pruebas

--- pensum-toolbox/tests/unit/utilities/OperacionesCreditosTest.php
+++ pensum-toolbox/tests/unit/utilities/OperacionesCreditosTest.php
@@ -33,6 +33,6 @@ class OperacionesCreditosTest extends \Codeception\Test\Unit
     }
     public function testNoMasDeSieteCreditosExtracurriculares(){
       $creditosExtra = OperacionesCreditos::get_total_creditosextra_usuario($this->usuario_de_pruebas);
-      $this->assertEquals($creditosExtra,7);
+      $this->assertTrue($creditosExtra <= 7);
     }
 }
