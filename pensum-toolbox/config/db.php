1a82f8bc794d1755be417bd58340411fbd86664a
fabiov24 <fabio.vasquez.24@gmail.com>
2018-10-18 17:03:23 -0600

    Historia 10

--- pensum-toolbox/config/db.php
+++ pensum-toolbox/config/db.php
@@ -2,9 +2,12 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=35.231.122.253;dbname=analisis',
-    'username' => 'analisis',
-    'password' => 'superuser123',
+    //'dsn' => 'mysql:host=35.231.122.253;dbname=analisis',
+    //'username' => 'analisis',
+    //'password' => 'superuser123',
+    'dsn' => 'mysql:host=localhost;dbname=analisis1',
+    'username' => 'root',
+    'password' => 'Mysql123@',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
