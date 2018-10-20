0b67d605e8e32c8f42e37ed22ec2a7a4261db051
david <dasensag@gmail.com>
2018-10-19 20:15:54 -0600

    Jenkisfile

--- pensum-toolbox/tests/_support/AcceptanceTester.php
+++ pensum-toolbox/tests/_support/AcceptanceTester.php
@@ -130,58 +130,6 @@ class AcceptanceTester extends \Codeception\Actor
         //throw new \Codeception\Exception\Incomplete("Step `I should not be able to signup` is not defined");
     }
 
-        /**
-     * @Given I want to fail math:num1
-     */
-     public function iWantToFailMath($num1)
-     {
-         $this->amOnPage("/index.php?r=site%2Fno-aprobar-curso&codigo_curso=101");
-         //throw new \Codeception\Exception\Incomplete("Step `I want to fail math:num1` is not defined");
-     }
-
-
-
-    /**
-     * @When I set push No Aprobar
-     */
-     public function iSetPushNoAprobar()
-     {
-     	$this->See('Marcado como no aprobado el curso: 101 Mate Basica 1');
-         //throw new \Codeception\Exception\Incomplete("Step `I set push No Aprobar` is not defined");
-     }
-
-    /**
-     * @Given I want to win math:num1
-     */
-     public function iWantToWinMath($num1)
-     {
-     	 $this->amOnPage("/index.php?r=site%2Faprobar-curso&codigo_curso=101");
-         //throw new \Codeception\Exception\Incomplete("Step `I want to win math:num1` is not defined");
-         //http://localhost:8080/index.php?r=site%2Faprobar-curso&codigo_curso=101
-     }
-
-    /**
-     * @When I set push Aprobar
-     */
-     public function iSetPushAprobar()
-     {
-     	 $this->See('Marcado como aprobado el curso: 101 Mate Basica 1');  
-         //throw new \Codeception\Exception\Incomplete("Step `I set push Aprobar` is not defined");
-     }
-
-        /**
-     * @Then I should be able to dashboard
-     */
-     public function iShouldBeAbleToDashboard()
-     {
-     	$this->See('Se actualiza dashboard');  
-         //throw new \Codeception\Exception\Incomplete("Step `I should be able to dashboard` is not defined");
-     }
-    public function iShouldAMessageOfSuccess()
-    {
-         $this->see("Marcado como aprobado la actividad");
-        //throw new \Codeception\Exception\Incomplete("Step `I should a message of success` is not defined");
-    }
     /**
      * @Given I want to see the courses
      */
